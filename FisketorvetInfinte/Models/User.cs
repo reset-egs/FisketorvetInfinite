c758a5e58001d26136f286352a3ca5d6f356676d
reset-egs <souleater12456@gmail.com>
2021-12-01 11:38:06 +0100

    Add - Basic login functionality

--- FisketorvetInfinte/Models/User.cs
+++ FisketorvetInfinte/Models/User.cs
@@ -2,11 +2,33 @@
 {
     public class User
     {
-        public int UserId { get; set; }
+        [Required, MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Password { get; set; }
+
         public string Name { get; set; }
+
         public string Address { get; set; }
+
         public string PhoneNumber { get; set; }
+
         public string Email { get; set; }
-        public bool IsAdministrator { get; set; }  
+
+        public bool IsAdministrator { get; set; }
+
+        public User FindUser(string username)
+        {
+            List<User> users = JsonFileReaderUser.ReadJson("./Data/Users.json");
+            foreach(var user in users)
+            {
+                if (user.Username == username)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
     }
 }
--- FisketorvetInfinte/Pages/Login.cshtml.cs
+++ FisketorvetInfinte/Pages/Login.cshtml.cs
@@ -5,8 +5,46 @@ namespace FisketorvetInfinte.Pages
 {
     public class Index1Model : PageModel
     {
+        [BindProperty]
+        public string username { get; set; }
+        [BindProperty]
+        public string password { get; set; }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public string Msg { get; set; }
+
+        public List<User> Users { get; set; } = JsonFileReaderUser.ReadJson("./Data/Users.json");
+
         public void OnGet()
         {
+            
+        }
+        public IActionResult OnPost()
+        {
+            if (User.FindUser(username) != null)
+            {
+                User = User.FindUser(User.Username);
+            } 
+            else
+            {
+                Msg = "Invalid";
+                return Page();
+            }
+            
+            if (User.Password == password)
+            {
+                HttpContext.Session.SetString("username", User.Username);
+                return RedirectToPage("Index");
+            }
+             
+            else
+            {
+                Msg = "Invalid";
+                return Page();
+            }
+            return Page();
         }
     }
 }
