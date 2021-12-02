ae5d038f281ef5a922118a64913f350158f36e65
F1-Guy <bartol.cagalj@gmail.com>
2021-12-02 17:37:53 +0100

    Fix - everything that the funky merge broke

---
+++ FisketorvetInfinte/Pages/Shop handling/EditShop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FisketorvetInfinte.Pages.Shop_handling
+{
+    public class EditShopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
