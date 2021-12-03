1c8bdc8681b1799c01eca29e93c0c67536bd7f34
F1-Guy <bartol.cagalj@gmail.com>
2021-12-03 12:56:05 +0100

    Add - Password hash class

---
+++ FisketorvetInfinte/Helpers/HashHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+namespace FisketorvetInfinte.Helpers
+{
+    public static class HashHelper
+    {
+            public static byte[] GetHash(string input)
+            {
+                using (HashAlgorithm algorithm = SHA256.Create())
+                    return algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+            }
+
+            public static string HashPassword(string password)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in GetHash(password))
+                    sb.Append(b.ToString("X2"));
+
+                return sb.ToString();
+            }
+    }
+}
--- FisketorvetInfinte/Pages/UserManagement/Register.cshtml.cs
+++ FisketorvetInfinte/Pages/UserManagement/Register.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FisketorvetInfinte.Helpers;
 
 namespace FisketorvetInfinte.Pages
 {
