10cbf63f23b4771f1b276ef95ded3a487cffd2e8
Digna Bringule <dbringule@gmail.com>
2021-12-03 11:35:49 +0100

    Add - user table

---
+++ FisketorvetInfinte/Pages/UserManagement/AllUsers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FisketorvetInfinte.Pages.UserManagement
+{
+    public class AllUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
