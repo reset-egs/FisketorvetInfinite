f3348f55b64ac4745b71170b2ded62ae80bd1d2b
reset-egs <souleater12456@gmail.com>
2021-12-07 11:10:25 +0100

    Add - events page and CRUD operations

---
+++ FisketorvetInfinte/Pages/EventManagement/EditEvent.cshtml.cs
@@ -0,0 +1,33 @@
+namespace FisketorvetInfinte.Pages.EventManagement
+{
+    public class EditEventModel : PageModel
+    {
+        public IEventService _eventService;
+
+        public EditEventModel(IEventService service)
+        {
+            _eventService = service;
+        }
+
+        public string Role { get; set; }
+
+        [BindProperty]
+        public Event Event { get; set; } = new Event();
+
+        public void OnGet(int id)
+        {
+            Role = HttpContext.Session.GetString("role");
+            Event = _eventService.FindEvent(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _eventService.UpdateEvent(Event);
+                return RedirectToPage("AllEvents");
+            }
+            return Page();
+        }
+    }
+}
